c94cb64f3c2e413aa2783ef6bb4b62d1de864385
Soeren MEDARD <soeren.medard@outlook.com>
2020-01-24 16:56:00 +0900

    api: error when acking a result already acked

--- api/lib/api_web/controllers/v1/task/ack_controller.ex
+++ api/lib/api_web/controllers/v1/task/ack_controller.ex
@@ -15,27 +15,39 @@ defmodule ApiWeb.V1.Task.AckController do
           limit: 1
       )
 
-    if is_nil(last_result) or not is_nil(last_result.acked_at) do
-      conn
-      |> Plug.Conn.put_status(400)
-      |> json(%{"message" => "No result or already acked"})
-    else
-      now = DateTime.utc_now() |> DateTime.truncate(:second)
+    cond do
+      is_nil(last_result) ->
+        conn
+        |> Plug.Conn.put_status(400)
+        |> json(%{"message" => "No result yet"})
 
-      result =
-        change(last_result, %{acked_at: now})
-        |> Repo.update()
+      not is_nil(last_result.acked_at) ->
+        conn
+        |> Plug.Conn.put_status(400)
+        |> json(%{"message" => "Already acked"})
 
-      case result do
-        {:ok, last_result} ->
-          ApiWorker.EventManager.ack(ApiWorker.EventManager, task_id, now)
-          json(conn, last_result)
+      is_nil(last_result.sent_at) ->
+        conn
+        |> Plug.Conn.put_status(400)
+        |> json(%{"message" => "Cannot ack a result which has not been sent yet"})
 
-        {:error, %{errors: errors}} ->
-          conn
-          |> Plug.Conn.put_status(400)
-          |> json(errors)
-      end
+      true ->
+        now = DateTime.utc_now() |> DateTime.truncate(:second)
+
+        result =
+          change(last_result, %{acked_at: now})
+          |> Repo.update()
+
+        case result do
+          {:ok, last_result} ->
+            ApiWorker.EventManager.ack(ApiWorker.EventManager, task_id, now)
+            json(conn, last_result)
+
+          {:error, %{errors: errors}} ->
+            conn
+            |> Plug.Conn.put_status(400)
+            |> json(errors)
+        end
     end
   end
 end
