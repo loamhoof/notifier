ff79954ded2afd016c401490a65d237a8ac8d4dd
Soeren MEDARD <soeren.medard@outlook.com>
2020-01-31 17:40:22 +0900

    core: fix migration

--- api/priv/repo/migrations/20190730055620_create_task_results.exs
+++ api/priv/repo/migrations/20190730055620_create_task_results.exs
@@ -5,7 +5,7 @@ defmodule Api.Repo.Migrations.CreateTaskResults do
     create table(:task_results) do
       add :load, :map, null: false
       add :sent, :boolean, default: false, null: false
-      add :task_id, null: false, references(:tasks, on_delete: :delete_all)
+      add :task_id, references(:tasks, on_delete: :delete_all), null: false
 
       timestamps()
     end
